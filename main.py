99e2cb0fd4dec856c5eb23d2d15cc94b08763299
Kayjey <kayjeyvinyl@gmail.com>
2026-09-17 13:17:01 -0400

    Added Python project

---
+++ main.py
@@ -0,0 +1,25 @@
+equation1 = input("Enter 5 multiplied by 6: ")
+answer1 = 5 * 6
+print("The answer to your query is:", answer1)
+
+equation2 = input("Enter 8 divided by 3: ")
+answer2 = 8 / 3
+print("The answer to your query is:", round(answer2, 8))
+
+equation3 = input("Enter 8 divided by 3 without the remainder: ")
+answer3 = 8 // 3
+print("The answer to your query is:", answer3)
+
+equation4 = input("Enter 8 modulo 3 (remainder only): ")
+answer4 = 8 % 3
+print("The answer to your query is:", answer4)
+
+equation5 = input("Enter 4200000 add 23090: ")
+answer5 = 4200000 + 23090
+print("The answer to your query is:", answer5)
+
+equation6 = input("Enter 4200000 subtract 7633: ")
+answer6 = 4200000 - 7633
+print("The answer to your query is:", answer6)
+
+print("Kimsy Jay Bastien")
